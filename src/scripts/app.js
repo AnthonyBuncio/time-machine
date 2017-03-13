a81e7434f25e30f9bb0dda3fba47fb24561e0c9e
AnthonyBuncio <anthonyb.ft@gmail.com>
2017-03-13 15:58:17 -0500

    buttons change color based on state

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,85 +4,95 @@ import Backbone from 'backbone'
 import init from './init'
 
 var date = new Date(),
-	year = date.getFullYear(),
-	interval = 1000
+	year = date.getFullYear()
 
 var TimeMachine = React.createClass({
+	//the state that year and headingTo begins as..
+	//will change as the buttons are clicked after first render
 	getInitialState : function() {
 		return {
 			year : year,
-			headingTo : ''
+			headingTo : '',
+			tickRate : 1000,
+			acceleration : 1
 		}
 	},
+
+	componentWillMount : function() {
+		this._tick()
+	},
+
+	//function to run once 'Future' button is clicked and state is changed to 'forward'
+	//setState will re-render the page with the year + 1; interval is also increased 
 	_forward : function () {
-		if (this.state.headingTo === 'forward') {
-			this.state.year += 1
-			this.setState({
-					year : this.state.year,
-					headingTo : this.state.headingTo
-				})
-			interval *= .8
-			this.forwardButton = setTimeout(this._forward, interval)
-		}
+		this.setState({
+			headingTo: 'forward',
+			acceleration: 0.85
+		})
 	},
+	//function to run once 'Past' button is clicked and state is changed to 'backward'
+	//setState will re-render the page with the year -1; interval also increased
 	_backward : function () {
+		this.setState({
+			headingTo: 'backward',
+			acceleration: 0.85
+		})
+	},
+	//changes state and resets the interval to 1000, before the increase
+	_stop : function () {
+		this.setState({
+			headingTo : 'stop',
+			tickRate: 1000
+		})
+	},	
+
+	_tick: function() {
+		var change = 0
+
 		if (this.state.headingTo === 'backward') {
-			this.state.year -= 1
-			this.setState({
-					year : this.state.year,
-					headingTo : this.state.headingTo
-				})
-			interval *= .8
-			this.backwardButton = setTimeout(this._backward, interval)
+			change = -1
 		}
-	},
-	stop : function () {
-		this.state.headingTo = 'stop'
+		else if (this.state.headingTo === 'forward') {
+			change = 1
+		}
+		else {
+			change = 0
+		}
 		this.setState({
-			headingTo : this.state.headingTo
-			})
-		interval = 1000
-	},
-	future : function () {
-		this.state.headingTo = 'forward'
-		this.setState({
-			headingTo : this.state.headingTo
-			})
-		this._forward()
-	},
-	past : function () {
-		this.state.headingTo = 'backward'
-		this.setState({
-			headingTo : this.state.headingTo
-			})
-		this._backward()
+			year: this.state.year + change,
+			tickRate: this.state.tickRate * this.state.acceleration
+		})
+		setTimeout(this._tick, this.state.tickRate)
 	},
 	
 	render : function() {
-		var forwardButton = ''
-		var backwardButton = ''
-		var stopButton = ''
+		var forwardButton = '',
+			backwardButton = '',
+			stopButton = '',
+			year = ''
 		if(this.state.headingTo === 'forward'){
-			forwardButton = 'forward'
+			forwardButton = 'forward',
+			year = 'forward-year'
 		}
 		else if(this.state.headingTo === 'backward'){
 			backwardButton = 'backward'
+			year = 'backward-year'
 		}
 		else if (this.state.headingTo === 'stop'){
 			stopButton = 'stop'
+			year = 'stopped-year'
 		}
 
-
 		return (
-				<div className="machine">
-					<h1>GOTTA BLAST..</h1>
-					<h2 className="year">{this.state.year}</h2>
-					<div className="nav"> 
-						<button className={backwardButton} onClick={this.past}>Past</button>
-						<button className={stopButton} onClick={this.stop}>Stop</button>
-						<button className={forwardButton} onClick={this.future}>Future</button>
-					</div>
+			<div className="machine">
+				<h1>GOTTA BLAST..</h1>
+				<h2 className={year}>{this.state.year}</h2>
+				<div className="nav"> 
+					<button className={backwardButton} onClick={this._backward}>Past</button>
+					<button className={stopButton} onClick={this._stop}>Stop</button>
+					<button className={forwardButton} onClick={this._forward}>Future</button>
 				</div>
+			</div>
 			)
 	}
 })
